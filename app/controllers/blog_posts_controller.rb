f70e51d86687d7624e9a3a6eea5b6648e425125d
Iain Macleod <macleod@live.co.uk>
2024-12-26 16:40:59 +0000

    Pre deploy check

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,47 @@
+class BlogPostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to blog_posts_path
+  end
+
+  private
+  def set_post
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+end
